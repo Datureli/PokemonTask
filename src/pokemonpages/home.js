518ae8437957865ea013d0053c8443b98c25e00a
Datureli <soleris1@gmail.com>
2022-03-17 18:26:51 +0100

    fix unexpected-token-in-json-at-position-0 error,missing https

--- src/pokemonpages/home.js
+++ src/pokemonpages/home.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import { useState, useEffect } from "react";
 const Home = () => {
-return(
-        <div>
-            <h1>Pokemon Lists</h1>
-        </div>
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [pokemons, setPokemons] = useState([]);
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon")
+      .then(async response => response.json())
+      .then(
+        (data) => {
+          setIsLoaded(true);
+          setPokemons(data);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, []);
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <ul>
+        {pokemons.map((pokemon) => (
+          <li key={pokemon.id}>{pokemon.name}</li>
+        ))}
+      </ul>
     );
-}
+  }
+};
 export default Home;
